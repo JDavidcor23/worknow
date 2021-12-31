a94468201c98a4c7cc2fbf038e563cf8eddc65c0
JDavidcor23 <jorgedavid23diaz@gmail.com>
2021-12-30 19:13:53 -0500

    estructura lista

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home.js";
+import Aliado from "./Pages/Aliado";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Aliado" element={<Aliado />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Aliado.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Aliado = () => {
+    return (
+        <div>
+           Aliado 
+        </div>
+    );
+};
+
+export default Aliado;
