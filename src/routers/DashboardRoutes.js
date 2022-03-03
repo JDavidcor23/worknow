32076439ea69c4941ef06d1ad7f7387d722e402f
Osmeily Pernia <osmeilypernia@gmail.com>
2022-03-03 12:14:21 -0500

    agregada estructura de archivos en process

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,6 +1,7 @@
 import {Routes, Route, Navigate
 } from 'react-router-dom';
 import Aliado from '../componentes/Aliado/Aliado';
+import FirstStep from '../componentes/process/FirstStep';
 
 
 const DashboardRoutes = () => {
@@ -8,6 +9,7 @@ const DashboardRoutes = () => {
         <Routes>
             <Route path="/aliado" element={<Aliado/>}/>
             <Route path='/*' element={<Navigate to="/" />} />
+            <Route path='/servicio-contratar' element={<FirstStep/>} />
         </Routes>
     );
 };
